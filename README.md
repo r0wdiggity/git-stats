c62b7bd27d13cc30e16f10e870d460ba003a9343
r0wdiggity <r0wdiggity1@gmail.com>
2024-08-16 13:35:15 -0500

    updated the readme

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # git-stats
 Github Statistics for the things you care about
+
+### Get the Enterprise GH App Token
+1. Get the Github App Id from your Github Enterprise Admin
+2. Get the PEM file from your Github Enterprise Admin
+3. Run teh following command to get the token
+```bash
+./access_token.sh <app_id> <pem_file>
+```
+
+### Run the Program
+1. To run the program from this repo run the following command
+```bash
+cargo run -- <args>
+```
+
+Running the help command will show the arguments:
+```bash
+cargo run -- --help
+```
+
+The arguments are as follows:
+```bash
+Usage: git-stats [OPTIONS] --owner <OWNER>
+
+Options:
+  -o, --owner <OWNER>  
+  -r, --repos <REPOS>  
+  -d, --date <DATE>    
+  -h, --help           Print help
+  -V, --version        Print version
+```
+
+Repos can be a single repository, or a comma separated list of repositories. The date is in the format of `YYYY-MM-DD`.
+Repos is optional, and if not provided, the program will default to all repositories in the organization.
---
+++ access_token.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+
+app_id=$1 # App ID as first argument
+pem=$( cat $2 ) # file path of the private key as second argument
+
+now=$(date +%s)
+iat=$((${now} - 60)) # Issues 60 seconds in the past
+exp=$((${now} + 600)) # Expires 10 minutes in the future
+
+b64enc() { openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n'; }
+
+header_json='{
+    "typ":"JWT",
+    "alg":"RS256"
+}'
+# Header encode
+header=$( echo -n "${header_json}" | b64enc )
+
+payload_json='{
+    "iat":'"${iat}"',
+    "exp":'"${exp}"',
+    "iss":'"${app_id}"'
+}'
+# Payload encode
+payload=$( echo -n "${payload_json}" | b64enc )
+
+# Signature
+header_payload="${header}"."${payload}"
+signature=$(
+    openssl dgst -sha256 -sign <(echo -n "${pem}") \
+    <(echo -n "${header_payload}") | b64enc
+)
+
+# Create JWT
+JWT="${header_payload}"."${signature}"
+printf '%s\n' "$JWT"
+
+#use the JWT token to get installations (the id#
+installid=$(curl -i -H "Authorization: Bearer $JWT" -H "Accept: application/vnd.github.v3+json" https://api.github.com/app/installations | grep html_url | grep installations | cut -d '/' -f 8 | cut -d '"' -f1)
+echo "installid: $installid"
+#Now get an ACCESS TOKEN
+accesstoken=$(curl -i -X POST -H "Authorization: Bearer $JWT" -H "Accept: application/vnd.github.v3+json" https://api.github.com/app/installations/$installid/access_tokens | grep token | cut -d '"' -f 4)
+echo "accesstoken: $accesstoken"
